27f8243139043d5da55e2e41235f69c7a27bf20f
Xuezhe Ma <xuezhema@discovery1.usc.edu>
2021-12-03 11:34:49 -0800

    update

--- exps_ttt/run.sh
+++ exps_ttt/run.sh
@@ -1,12 +1,13 @@
 #! /bin/bash
 #SBATCH --output=slurm_logs/slurm-%A-%a.out
 #SBATCH --error=slurm_logs/slurm-%A-%a.err
-#SBATCH --job-name=xsum.lora.init.4.comb
+#SBATCH --job-name=rte.val.token.entropy
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:A6000:4
-#SBATCH --mem=100g
-#SBATCH --cpus-per-task=5
-#SBATCH --time=0
+#SBATCH --gres=gpu:a40:1
+#SBATCH --mem=60g
+#SBATCH -p isi
+#SBATCH --cpus-per-task=6
+#SBATCH --time=96:00:00
 ##SBATCH --array=0
 
 module load gcc/8.3.0
@@ -36,38 +37,58 @@ DATE=`date +%Y%m%d`
 
 dataset="super_glue"
 subset="rte"
-pbsz=2
+bsz=1
+nprompts=5
 testset_name="validation"
 
 peft="prompt_tuning"
-pL=3
-lr=3e-5
+peft="lora"
+pL=1
+lora_pos="encdec"
+
+lr=1e-5
 lr_scheduler_type="polynomial"
-max_steps=50
+max_steps=1000
 max_epochs=50
 log_steps=10
 debugsize=-1
 
-test_mode="ttt_t0"
-model="bigscience/T0pp"
+loss_opt="token_level_entropy"  # consistency, token_level_entropy, entropy
+#loss_opt="entropy"
+temp=1.0
+copt="uniform"
 
-exp_name=${test_mode}.${dataset}.${subset}.${testset_name}.${model}
+test_mode="ttt_t0"
+train_data="validation"  # test, stream
+#train_data="train"  # test, stream
+model="T0_3B"
+
+exp_name=${test_mode}.train.source.${train_data}.${dataset}.${subset}.${testset_name}.${model}.peft.${peft}.bn${pL}.lora_pos.${lora_pos}.lopt.${loss_opt}.combine.${copt}.temp.${temp}
 SAVE=checkpoints/${dataset}/${DATE}/${exp_name}
 rm -rf ${SAVE}; mkdir -p ${SAVE}
 cp ${0} ${SAVE}/run.sh
 
-deepspeed --num_gpus=4 examples/pytorch/t0-zero-shot/run_t0.py \
-  --deepspeed deepspeed_configs/ds_config.json \
+#deepspeed --num_gpus=1 
+#python -u 
+#python -m torch.distributed.launch --nproc_per_node 4 
+#CUDA_VISIBLE_DEVICES=0
+python -u examples/pytorch/t0-zero-shot/run_t0.py \
   --dataset_name ${dataset} --subset_name ${subset} --prompt_set_name ${dataset} --testset_name ${testset_name} \
-  --model_name_or_path ${model} --per_device_train_batch_size ${pbsz}  --per_device_eval_batch_size 10 \
+  --model_name_or_path ${model} --per_device_train_batch_size ${bsz}  --per_device_eval_batch_size 10 \
   --test_mode ${test_mode} --cache_dir ${cache_dir} \
   --debug_size ${debugsize} \
-  --peft_option ${peft} --prompt_tuning_L ${pL} \
+  --peft_option ${peft} --bottleneck_dim ${pL} \
+  --lora_pos 'dec' --lora_alpha 4 \
   --do_train --logging_steps ${log_steps} --num_train_epochs ${max_epochs} --max_steps ${max_steps} \
   --adam_beta1 0.9 \
   --adam_beta2 0.98 \
   --adam_epsilon 1e-6 \
   --learning_rate ${lr} \
+  --loss_option ${loss_opt} \
+  --lora_dropout 0.2 --lora_alpha 4 --lora_pos ${lora_pos} \
+  --prob_temperature ${temp} --combine_option ${copt} \
+  --train_random_n_prompts ${nprompts} --train_data_source ${train_data} \
+  --save_strategy "no" --warmup_steps 100 --gradient_accumulation_steps 16 \
   --lr_scheduler_type ${lr_scheduler_type} \
-  --output_dir ${SAVE} --overwrite_output_dir --fp16 --report_to "none" \
+  --output_dir ${SAVE} --overwrite_output_dir --report_to "none" \
   --disable_tqdm "True" 2>&1 | tee ${SAVE}/log.txt
