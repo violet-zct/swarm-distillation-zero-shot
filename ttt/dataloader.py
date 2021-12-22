8dfbee97ff1ca6de179d743f9ca1054ae7b306f6
Chunting Zhou <chuntingzhou@Chuntings-MacBook-Pro.local>
2021-12-21 21:38:53 -0500

    fix copa prompts

--- ttt/dataloader.py
+++ ttt/dataloader.py
@@ -24,7 +24,7 @@ class DatasetByPrompt(Dataset):
 
         self.tokenizer = tokenizer
         self.prompts = DatasetTemplates(self.PROMPTSET_NAME, self.SUBSET_NAME)
-        self.original_task_prompts = self.extract_original_task_prompts()
+        self.original_task_prompts = self.extract_original_task_prompts(check_valid_prompts=self.SUBSET_NAME=="copa")
         self.construct_meta_info()
         self.num_choices = len(self.prompts[self.original_task_prompts[0]].get_answer_choices_list(self.dataset[0]))
         print("{} has {} original task prompts, number choices = {}, total test examples = {}".format(self.DATASET_NAME + ("/" + self.SUBSET_NAME) if self.SUBSET_NAME is not None else "",
@@ -91,15 +91,19 @@ class DatasetByPrompt(Dataset):
         assert check
         self.num_choices = n
 
-    def extract_original_task_prompts(self):
+    def extract_original_task_prompts(self, check_valid_prompts=False):
         all_prompt_names = self.prompts.all_template_names
-        valid_prompts = []
-        for name in all_prompt_names:
-            if self.prompts[name].metadata.original_task:
-                apply_length = len(self.prompts[name].apply(self.dataset[0]))
-                if apply_length == 2:
-                    valid_prompts.append(name)
-        return valid_prompts
+        invalid_names = []
+        if check_valid_prompts:
+            for pname in all_prompt_names:
+                if self.prompts[pname].metadata.original_task:
+                    for d in self.dataset:
+                        return_value_length = len(self.prompts[pname].apply(d))
+                        if return_value_length != 2:
+                            invalid_names.append(pname)
+                            break
+        invalid_names = set(invalid_names)
+        return [name for name in all_prompt_names if self.prompts[name].metadata.original_task and name not in invalid_names]
 
 
 class TTTOnlineDataset(Dataset):
